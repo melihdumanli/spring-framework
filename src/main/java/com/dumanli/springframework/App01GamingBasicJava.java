9dd478f815471fd5e4b0081bd769b5245e5aad3c
Melih DUMANLI <melihdumanli@hotmail.com>
2023-03-28 21:39:41 +0300

    Launching a Spring Context

--- src/main/java/com/dumanli/springframework/App01GamingBasicJava.java
+++ src/main/java/com/dumanli/springframework/App01GamingBasicJava.java
@@ -1,21 +1,21 @@
 package com.dumanli.springframework;
 
 import com.dumanli.springframework.game.GameRunner;
-import com.dumanli.springframework.game.MarioGame;
 import com.dumanli.springframework.game.PacManGame;
 import com.dumanli.springframework.game.SuperContraGame;
 
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
 
         //var game = new MarioGame();
         var game = new SuperContraGame();
         // GameRunner class is no longer coupled to a specific game.
         // But only coupled to the GamingConsole
-        var game1 = new PacManGame();
+        var game1 = new PacManGame(); // 1: Object Creation
         var gameRunner = new GameRunner(game);
-        var gameRunner2 = new GameRunner(game1);
+        var gameRunner2 = new GameRunner(game1); // 2: Object Creation + Wiring of Dependencies
+            // Game is dependency of GameRunner
         gameRunner.run();
         gameRunner2.run();
     }
