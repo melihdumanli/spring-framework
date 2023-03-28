9adcc3dbeb837033d87633616768a2513bcfd31b
Melih DUMANLI <melihdumanli@hotmail.com>
2023-03-28 21:48:38 +0300

    Creating Beans

--- src/main/java/com/dumanli/springframework/HelloWorldConfiguration.java
+++ src/main/java/com/dumanli/springframework/HelloWorldConfiguration.java
@@ -3,10 +3,27 @@ package com.dumanli.springframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {}
+record Address (String fistLine, String city) {}
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name(){
         return "Melih";
+    }
+
+    @Bean
+    public int age(){
+        return 22;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Melih", 22);
+    }
+
+    @Bean
+    public Address address(){
+        return new Address("Baker Street", "London");
     }
 }
